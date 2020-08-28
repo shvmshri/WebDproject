644c89cab8709f96cbec9d9b0ecaa97c019d1fed
shvmshri <shvmshri@iitk.ac.in>
2020-08-29 02:54:41 +0530

    _

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine','ejs');
-
+mongoose.connect("mongodb+srv://Shivamshree:Myclusterpassword@cluster0.ncnhl.mongodb.net/IITK",{
+    useNewUrlParser:true,useUnifiedTopology:true
+});
 
 app.get("/",function(req,res){
     res.render("home");
@@ -26,19 +28,39 @@ app.get("/lostfound",function(req,res){
 })
 
 // Cab share
+const cabSchema = new mongoose.Schema({
+    Date:String,
+    Name:String,
+    Email:String,
+    Phone_no:String,
+    Destination:String,
+    Departure_time:String
+});
+const Cab = new mongoose.model('cab',cabSchema);
+
 app.get("/cabshare",function(req,res){
     res.render("Cab-Share");
 });
 app.post("/cabshare",function(req,res){
+    const cab = new Cab({
+        Date:req.body.date,
+        Name:req.body.name,
+        Email:req.body.email,
+        Phone_no:req.body.phone_no,
+        Destination:req.body.destination,
+        Departure_time:req.body.departure_time
+    });
+    cab.save();
+    res.redirect("/cabshare");
     console.log(req.body);
 })
 
 app.get("/cabshare/offer",function(req,res){
     res.render('offer');
 });
-
-
-
+app.get("/cabshare/find",function(req,res){
+    res.render("findcab");
+})
 
 
 app.listen(3000,function(){
